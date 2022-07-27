3319b562e08dcbb5c018b6afc4fbef8b5eaca6f0
edwarfmartinez <efmartinez35@gmail.com>
2022-07-27 09:18:04 -0500

    Initial Commit

---
+++ SpaceFlight/SpaceFlightApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceFlightApp.swift
+//  SpaceFlight
+//
+//  Created by EDWAR FERNANDO MARTINEZ CASTRO on 27/07/22.
+//
+
+import SwiftUI
+
+@main
+struct SpaceFlightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
